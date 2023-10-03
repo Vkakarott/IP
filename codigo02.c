1092285d462776451be4dfff41c81b4eef312a5c
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2023-10-03 16:08:50 -0300

    Create codigo02.c

---
+++ codigo02.c
@@ -0,0 +1,23 @@
+//lib input e outpt
+#include <stdio.h>
+
+int main() {
+	//Declarar variaveis
+    int i = 1;
+	float notas, media, peso;
+    float somaPesos = 0;
+
+    //loop de repetição
+    for(i = 1; i < 5; i++){
+        printf("nota %d: ", i);
+	    scanf("%f", &notas);
+        printf("Peso da nota %d: ", i);
+        scanf("%f", &peso);
+        media = media + notas * peso;
+        somaPesos = somaPesos + peso;
+    }
+	
+    media = media / somaPesos;
+
+	printf("A media entre as notas é %.2f\n", media);
+}
