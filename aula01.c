2630f03de8d14ab4742c1fd61ddef90e15677f67
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2023-10-03 15:04:47 -0300

    Update aula01.c

--- aula01.c
+++ aula01.c
@@ -1,25 +1,33 @@
 //importação de bibliotecas
 #include <stdio.h>
+//Definir constante
+#define PI 2;
 
 //Declaração do meu bloco principal
 int main(){
     //Declaração de variaveis
-    int n1, n2, s, m;
+    int n1, n2, s, m, dn1, dn2;
     printf("Digite um numero: ");
     //Entrada de valores para a variavel n1
-    scanf("%d", &n1);
+    scanf("%d", &n1); //& é nescessario para indicar o endereço da variavel a ser alterada
     printf("Digite outro numero: ");
     scanf("%d", &n2);
     s = n1 + n2;
     m = n1 * n2;
+    dn1 = n1 * PI;
+    dn2 = n2 * PI;
     printf("A soma é: %d", s);
     printf("\nA multiplicação é: %d \n", m);
+    printf("O dobro de %d e %d, são %d, %d respectivamente", n1, n2, dn1, dn2);
 }
 
 /*
 #include -> importa bibliotecas
+#define -> define constantes
 <stdio.h> -> biblioteca de input e output
-%d -> indica que printf() deve colocar um inteiro na tela
+%d , %i -> indica que printf() deve colocar um inteiro na tela
+%c -> caracteres
+%f -> float
 printf -> mostrar na tela
 scanf -> comando que recebe uma entrada do teclado
 
