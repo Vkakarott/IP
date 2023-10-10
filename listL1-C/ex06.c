29014775f730c4bcdc30ad70e5c0cf187e728f20
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2023-10-09 23:16:55 -0300

    07 error

--- listL1-C/ex06.c
+++ listL1-C/ex06.c
@@ -3,12 +3,14 @@
 #include <stdio.h>
 
 int main(){
-    int n;
+    int n, i;
     double s;
 
     scanf("%d", &n);
     if(n>1){
-        s = ;
+        for(i = 1; i <= n; i++){
+            s += (double)1/i;
+        }
         printf("%.6lf\n", s);
     } else {
         printf("Numero invalido!");
---
+++ listL1-C/ex07.c
@@ -0,0 +1,38 @@
+// TURMA DE INTRODUCAO A PROGRAMACAO
+
+#include <stdio.h>
+
+int main(){
+    int m, f, fn;
+    double p1, p2, p3, p4, p5, p6, p7, p8, l1, l2, l3, l4, l5, pf, nf, ch;
+    char *arr[] = {
+        "APROVADO",
+        "REPROVADO POR FREQUENCIA",
+        "REPROVADO POR NOTA",
+        "REPROVADO POR NOTA E POR FREQUENCIA"
+    };
+
+    do {
+        scanf("%d %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %d", &m, &p1, &p2, &p3, &p4, &p5, &p6, &p7, &p8, &l1, &l2, &l3, &l4, &l5, &pf, &f);
+
+        if(m<1){
+            break;
+        }
+
+        nf = 0.7*((p1+p2+p3+p4+p5+p6+p7+p8)/8)+0.15*((l1+l2+l3+l4+l5)/5)+0.15*pf;
+
+        ch = f*100/128;
+
+        if(nf >= 6 && ch >= 75){
+            fn = 0;
+        } else if (nf >= 6 && ch < 75){
+            fn = 1;
+        } else if (nf < 6 && ch >= 75){
+            fn = 2;
+        } else if(nf < 6 && ch < 75){
+            fn - 3;
+        }
+
+        printf("Matricula: %d, Nota Final: %.2lf, Situacao Final: %s\n", m, nf, arr[fn]);
+    } while (m>1);
+}
