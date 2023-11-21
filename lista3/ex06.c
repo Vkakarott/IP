96a9362371850ebd0c24d7c2ce8c40b5b11a3284
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2023-11-21 11:53:48 -0300

    fim

--- lista3/ex06.c
+++ lista3/ex06.c
@@ -2,18 +2,18 @@
 #include <string.h>
 
 int main(){
-char str[201];
-int i;
+    char str[201];
+    int i;
 
-while(scanf("%s", str) != EOF){
-int j = strlen(str);
-int palindromo = 1;
-for(i = 0; i < (j/2); i++){
-if(str[i] != Str[j-i]{
-palindromo = O;
-}
-}
-if(palindromo) printf("sim\n");
-else printf("não \n");
-}
+    while(scanf("%s", str) != EOF){
+        int j = strlen(str);
+        int palindromo = 1;
+        for(i = 0; i < (j/2); i++){
+            if(str[i] != str[j-i-1]){
+                palindromo = 0;
+            }
+        }
+        if(palindromo) printf("sim\n");
+        else printf("nao \n");
+    }
 }
--- lista3/ex14.c
+++ lista3/ex14.c
@@ -4,23 +4,40 @@
 #include <string.h>
 #include <ctype.h>
 
-int main(){
+int main() {
     int i;
     char str[51];
 
-    while(scanf(" %[^\n]", str) != EOF){
+    // Loop continua até o final do arquivo (EOF)
+    while (fgets(str, sizeof(str), stdin) != NULL) {
         int M = 1;
-        for (i = 0; i < strlen(str); i++){
-            if (isalpha(str[i])){
-                if (M){
+        int length = strlen(str);
+
+        // Remover o caractere de nova linha, se presente
+        if (length > 0 && str[length - 1] == '\n') {
+            str[length - 1] = '\0';
+            length--;
+        }
+
+        // Loop através de cada caractere na string de entrada
+        for (i = 0; i < length; i++) {
+            // Verifica se o caractere é alfabético
+            if (isalpha(str[i])) {
+                // Se M for 1 (verdadeiro), converte o caractere para maiúsculas
+                if (M) {
                     str[i] = toupper(str[i]);
-                    M = 0;
+                    M = 0; // Define M como 0 (falso) para a próxima iteração
                 } else {
+                    // Se M for 0 (falso), converte o caractere para minúsculas
                     str[i] = tolower(str[i]);
-                    M = 1;
+                    M = 1; // Define M como 1 (verdadeiro) para a próxima iteração
                 }
             }
         }
+
+        // Imprime a string modificada
         printf("%s\n", str);
     }
+
+    return 0; // Indica execução bem-sucedida do programa
 }
